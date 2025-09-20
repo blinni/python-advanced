a3a64cf976f78862648d8562b3ac13a8aa05e1de
Blini nasufi <blininasufi820@gmail.com>
2025-09-20 14:54:21 +0200

    module, libraries, package..

---
+++ main2.py
@@ -0,0 +1,9 @@
+import math as m
+result = m.sqrt(81)
+print(result)
+
+#second way ///
+
+from math import sqrt as sq
+result = sq(36)
+print(result)
---
+++ my_pacgage/main.py
@@ -0,0 +1,8 @@
+from PIL.ImageChops import difference
+from simple_math.math_utils import add, sub
+
+sum = add(a:8, b:5)
+difference = sub(a;8,b:5)
+
+print(sum)
+print(difference)
